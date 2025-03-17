bdb325b527ca884cb7e558126b545f0d75f8e98a
Giovanni Carlino <97813246+giovannicarlino04@users.noreply.github.com>
2025-03-17 15:59:40 +0100

    We

--- Stream.h
+++ Stream.h
@@ -1,219 +1,98 @@
-/**
- * Stream.h - Consolidated stream classes for XVPatcher
- * This file combines functionality from:
- * - Stream.h
- * - MemoryStream.h
- * - FileStream.h
- * - FixedMemoryStream.h
- * - FixedBitStream.h
- * - BitStream.h
- * - CrilaylaFixedBitStream.h
- */
+#ifndef __STREAM_H__
+#define __STREAM_H__
 
-#ifndef STREAM_H
-#define STREAM_H
+#include "BaseFile.h"
+typedef __int64 off64_t; // For Windows
 
-#include <cstdint>
-#include <string>
-#include <vector>
-#include <fstream>
-#include <memory>
-
-// For SEEK_* constants and compatibility
-#include <stdio.h>
-
-// Visual C++ compatibility
-#ifdef _MSC_VER
-    // Ensure __declspec(dllexport) is defined properly
-    #ifdef BUILDING_DLL
-        #define STREAM_API __declspec(dllexport)
-    #else
-        #define STREAM_API __declspec(dllimport)
-    #endif
-#else
-    #define STREAM_API
-#endif
-
-//================================================
-// Base Stream Class
-//================================================
-
-class STREAM_API Stream
+class Stream : public BaseFile
 {
-protected:
-    bool big_endian;
-    
 public:
-    Stream();
-    virtual ~Stream();
-    
-    virtual size_t Read(void *buf, size_t size) = 0;
-    virtual size_t Write(const void *buf, size_t size) = 0;
-    virtual size_t Tell() = 0;
-    virtual size_t Seek(size_t offset, int whence) = 0;
-    virtual size_t GetSize() = 0;
-    virtual size_t GetAvailable() = 0;
-    
-    bool IsBigEndian() const { return big_endian; }
-    void SetEndianness(bool big) { big_endian = big; }
-    
-    uint8_t Read8();
-    uint16_t Read16();
-    uint32_t Read32();
-    uint64_t Read64();
-    
-    void Write8(uint8_t val);
-    void Write16(uint16_t val);
-    void Write32(uint32_t val);
-    void Write64(uint64_t val);
-    
-    float ReadFloat();
-    void WriteFloat(float val);
-    
-    std::string ReadString(size_t size);
-    std::string ReadCString();
-    void WriteString(const std::string &str, bool null_terminated = false);
+
+    enum class Hash
+    {
+        NONE,
+        SHA1
+    };
+
+    enum class Cipher
+    {
+        NONE,
+        AES_PLAIN,
+    };
+
+    virtual uint64_t GetSize() const = 0;
+    virtual bool Resize(uint64_t size) = 0;
+    virtual bool Grow(int64_t size) { return Resize(GetSize()+size); }
+
+    virtual bool Read(void *buf, size_t size) = 0;
+    virtual bool Write(const void *buf, size_t size) = 0;
+
+	inline bool Read8(uint8_t *ptr)	{ return Read(ptr, sizeof(uint8_t)); }
+    inline bool Read8(int8_t *ptr) { return Read8((uint8_t *)ptr); }
+
+    inline bool Read16(uint16_t *ptr)
+    {
+        bool ret = Read(ptr, sizeof(uint16_t));
+        if (ret)
+            *ptr = val16(*ptr);
+
+        return ret;
+    }
+
+    inline bool Read16(int16_t *ptr) { return Read16((uint16_t *)ptr); }
+
+    inline bool Read32(uint32_t *ptr)
+    {
+        bool ret = Read(ptr, sizeof(uint32_t));
+        if (ret)
+            *ptr = val32(*ptr);
+
+        return ret;
+    }
+
+    inline bool Read32(int32_t *ptr) { return Read32((uint32_t *)ptr); }
+
+    inline bool Read64(uint64_t *ptr)
+    {
+        bool ret = Read(ptr, sizeof(uint64_t));
+        if (ret)
+            *ptr = val64(*ptr);
+
+        return ret;
+    }
+
+    inline bool Read64(int64_t *ptr) { return Read64((uint64_t *)ptr); }
+
+	inline bool ReadGuid(uint8_t *ptr) { return Read(ptr, 16); }
+
+    inline bool Write8(uint8_t value)	{ return Write(&value, sizeof(uint8_t)); }
+
+    inline bool Write16(uint16_t value)
+    {
+        value = val16(value);
+        return Write(&value, sizeof(uint16_t));
+    }
+
+    inline bool Write32(uint32_t value)
+    {
+        value = val32(value);
+        return Write(&value, sizeof(uint32_t));
+    }
+
+    inline bool Write64(uint64_t value)
+    {
+        value = val64(value);
+        return Write(&value, sizeof(uint64_t));
+    }
+
+	inline bool WriteGuid(const uint8_t *ptr)	{ return Write(ptr, 16); }
+
+    virtual bool Seek(off64_t offset, int whence) = 0;
+    virtual uint64_t Tell() = 0;
+
+    virtual bool Copy(Stream *other, size_t size);
+    virtual bool CopyEx(Stream *other, size_t size, Hash hash_mode=Hash::NONE, uint8_t *hash=nullptr, Cipher decrypt_mode=Cipher::NONE, const uint8_t *decrypt_key=nullptr, int decrypt_key_size=0, Cipher encrypt_mode=Cipher::NONE, const uint8_t *encrypt_key=nullptr, int encrypt_key_size=0);
+    virtual bool Align(unsigned int alignment);
 };
 
-//================================================
-// Memory Stream
-//================================================
-
-class STREAM_API MemoryStream : public Stream
-{
-protected:
-    uint8_t *buf;
-    size_t size;
-    size_t position;
-    
-public:
-    MemoryStream();
-    virtual ~MemoryStream();
-    
-    virtual size_t Read(void *buf, size_t size) override;
-    virtual size_t Write(const void *buf, size_t size) override;
-    virtual size_t Tell() override;
-    virtual size_t Seek(size_t offset, int whence) override;
-    virtual size_t GetSize() override;
-    virtual size_t GetAvailable() override;
-    
-    uint8_t *GetMemory(bool unlink = false);
-    void Resize(size_t new_size);
-};
-
-//================================================
-// Fixed Memory Stream
-//================================================
-
-class STREAM_API FixedMemoryStream : public Stream
-{
-protected:
-    uint8_t *buf;
-    size_t size;
-    size_t position;
-    bool is_owner;
-    
-public:
-    FixedMemoryStream(void *buf, size_t size, bool take_ownership = false);
-    virtual ~FixedMemoryStream();
-    
-    virtual size_t Read(void *buf, size_t size) override;
-    virtual size_t Write(const void *buf, size_t size) override;
-    virtual size_t Tell() override;
-    virtual size_t Seek(size_t offset, int whence) override;
-    virtual size_t GetSize() override;
-    virtual size_t GetAvailable() override;
-    
-    const uint8_t *GetMemory() const { return buf; }
-};
-
-//================================================
-// File Stream
-//================================================
-
-class STREAM_API FileStream : public Stream
-{
-protected:
-    std::fstream file;
-    std::string filename;
-    size_t size;
-    
-public:
-    FileStream();
-    ~FileStream();
-    
-    bool Open(const std::string &filename, bool read_only = false);
-    void Close();
-    
-    virtual size_t Read(void *buf, size_t size) override;
-    virtual size_t Write(const void *buf, size_t size) override;
-    virtual size_t Tell() override;
-    virtual size_t Seek(size_t offset, int whence) override;
-    virtual size_t GetSize() override;
-    virtual size_t GetAvailable() override;
-    
-    const std::string &GetFileName() const { return filename; }
-};
-
-//================================================
-// Bit Stream
-//================================================
-
-class STREAM_API BitStream
-{
-protected:
-    const uint8_t *buf;
-    size_t bit_position;
-    size_t size_in_bits;
-    
-public:
-    BitStream(const void *buf, size_t size_in_bits);
-    virtual ~BitStream();
-    
-    uint32_t GetBits(int num_bits);
-    size_t Tell() const { return bit_position; }
-    void Seek(size_t bit_pos);
-    size_t GetSize() const { return size_in_bits; }
-};
-
-//================================================
-// Fixed Bit Stream
-//================================================
-
-class STREAM_API FixedBitStream
-{
-protected:
-    const uint8_t *buf;
-    size_t bit_position;
-    size_t size_in_bits;
-    size_t size_in_bytes;
-    
-public:
-    FixedBitStream(const void *buf, size_t size_in_bits);
-    virtual ~FixedBitStream();
-    
-    uint32_t GetBits(int num_bits);
-    size_t Tell() const { return bit_position; }
-    void Seek(size_t bit_pos);
-    size_t GetSize() const { return size_in_bits; }
-};
-
-//================================================
-// Crilayla Fixed Bit Stream
-//================================================
-
-class STREAM_API CrilaylaFixedBitStream : public FixedBitStream
-{
-private:
-    bool result_ready;
-    uint32_t result;
-    
-public:
-    CrilaylaFixedBitStream(const void *buf, size_t size_in_bits);
-    ~CrilaylaFixedBitStream();
-    
-    uint32_t ReadBits(int num_bits);
-    void ReadNextBits();
-};
-
-#endif // STREAM_H
+#endif /* __STREAM_H__ */
